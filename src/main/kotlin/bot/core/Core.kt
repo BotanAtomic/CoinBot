b957b302aee13067d5f10d75c7dd944e38d828d8
botan <botan.david94@gmail.com>
2020-08-24 23:56:59 +0200

    Add possibility to create instance with constructor

--- src/main/kotlin/bot/core/Core.kt
+++ src/main/kotlin/bot/core/Core.kt
@@ -6,17 +6,22 @@ import bot.api.Connector
 import bot.api.Service
 import bot.channels.GenericChannel
 import bot.connectors.GenericConnector
+import bot.entity.User
 import bot.helper.loadClasses
 
 class Core {
 
-    val channels: List<GenericChannel> = loadClasses("bot.channels.impl", Channel::class.java)
-    val connectors: List<GenericConnector> = loadClasses("bot.connectors.impl", Connector::class.java)
-    val applications: List<Service> = loadClasses("bot.app", Application::class.java)
+    val channels: List<GenericChannel> = loadClasses("bot.channels.impl", Channel::class, this)
+    private val connectors: List<GenericConnector> = loadClasses("bot.connectors.impl", Connector::class)
+    private val applications: List<Service> = loadClasses("bot.app", Application::class)
 
     init {
         applications.forEach { it.start(this) }
         println("Successfully loaded ${channels.size} channels, ${connectors.size} connectors, ${applications.size} applications")
+    }
+
+    fun onReceiveCommand(message: String, user: User, source: String) {
+
     }
 
 }
--- src/main/kotlin/bot/helper/ReflectionHelper.kt
+++ src/main/kotlin/bot/helper/ReflectionHelper.kt
@@ -1,12 +1,24 @@
 package bot.helper
 
 import com.google.common.reflect.ClassPath
+import kotlin.reflect.KClass
 
 
-inline fun <reified T> loadClasses(packageName: String, annotation: Class<out Annotation>): List<T> {
+inline fun <reified T> loadClasses(
+    packageName: String,
+    annotation: KClass<out Annotation>,
+    vararg args: Any?
+): List<T> {
     val classPath = ClassPath.from(ClassLoader.getSystemClassLoader())
 
     return classPath.getTopLevelClasses(packageName).filter {
-        it.load().getAnnotationsByType(annotation) != null
-    }.map { it.load().getConstructor().newInstance() as T }.toList()
+        it.load().getAnnotationsByType(annotation.java) != null
+    }.map {
+        val constructor = it.load().constructors[0]
+        if (constructor.parameterCount > 0)
+            it.load().constructors[0].newInstance(*args) as T
+        else
+            it.load().constructors[0].newInstance() as T
+    }.toList()
 }
+
