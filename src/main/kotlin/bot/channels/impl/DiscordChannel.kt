648ac47d1b2ff9a1959744f50e2d738a31df18bb
botan <botan.david94@gmail.com>
2020-08-24 23:04:19 +0200

    Implement discord channel

--- src/main/kotlin/bot/channels/impl/DiscordChannel.kt
+++ src/main/kotlin/bot/channels/impl/DiscordChannel.kt
@@ -2,12 +2,35 @@ package bot.channels.impl
 
 import bot.api.Channel
 import bot.channels.GenericChannel
+import bot.events.DefaultEventListener
+import bot.helper.getTextChannel
+import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.entities.Activity
+import net.dv8tion.jda.api.utils.Compression
+import net.dv8tion.jda.api.utils.cache.CacheFlag
 
 @Channel
 class DiscordChannel : GenericChannel {
 
-    override fun send(message: String, channel: String) {
+    private val jda: JDA
 
+    init {
+        JDABuilder.createDefault(System.getenv("coin_bot_token")).apply {
+            disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE)
+            setBulkDeleteSplittingEnabled(false)
+            setCompression(Compression.NONE)
+            setActivity(Activity.watching("xnxx.com"))
+            addEventListeners(DefaultEventListener())
+            jda = build()
+        }
+    }
+
+    override fun send(message: String, channel: String) {
+        jda.getTextChannel(channel).apply {
+            sendTyping()
+            sendMessage(message).queue()
+        }
     }
 
 }
