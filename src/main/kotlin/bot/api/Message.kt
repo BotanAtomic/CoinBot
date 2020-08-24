a43beafeac19ed438bc17f656f57ada1da6b9849
botan <botan.david94@gmail.com>
2020-08-25 01:42:48 +0200

    Add description to Message (to build help command)

--- src/main/kotlin/bot/api/Message.kt
+++ src/main/kotlin/bot/api/Message.kt
@@ -2,4 +2,4 @@ package bot.api
 
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class Message(val header: String)
+annotation class Message(val value: String, val description: String)
---
+++ src/main/kotlin/bot/messages/impl/Help.kt
@@ -0,0 +1,32 @@
+package bot.messages.impl
+
+import bot.api.Message
+import bot.channels.GenericChannel
+import bot.helper.getClasses
+import bot.messages.CommandHandler
+import bot.messages.MessageData
+import java.lang.StringBuilder
+
+@Message("--help", "list all available commands")
+class Help : CommandHandler {
+
+    private val commands = getClasses("bot.messages.impl", Message::class).map {
+        it.getAnnotation(Message::class.java)
+    }.associateBy({ it.value }, { it.description })
+
+    override fun handle(message: MessageData, source: GenericChannel) {
+        val builder = StringBuilder("```JavaScript\n")
+
+        builder.append("CoinBot command usage: B <command> [<args>]\n\n")
+
+        val maxCommandLength = commands.keys.maxByOrNull { it.length }.orEmpty().length + 2
+
+        commands.forEach {
+            builder.append(it.key.padEnd(maxCommandLength)).append(": ").append(it.value).append("\n")
+        }
+
+        builder.append("```")
+        source.send(builder.toString(), message.source)
+    }
+
+}
