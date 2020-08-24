6658b087b3b30230aef2a8dfefa062cb4fce3483
botan <botan.david94@gmail.com>
2020-08-25 01:02:58 +0200

    Modify discord helper to get channel by id if needed

--- src/main/kotlin/bot/helper/DiscordHelper.kt
+++ src/main/kotlin/bot/helper/DiscordHelper.kt
@@ -2,7 +2,12 @@ package bot.helper
 
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.entities.TextChannel
+import net.dv8tion.jda.api.entities.User
 
-fun JDA.getTextChannel(name: String): TextChannel? =
-    if (this.textChannels.isEmpty()) null else this.textChannels.first { it.name == name }
+fun JDA.getTextChannel(id: String): TextChannel? =
+    if (this.textChannels.isEmpty()) null else when {
+        id.toLongOrNull() != null -> this.getTextChannelById(id.toLong())
+        else -> this.textChannels.firstOrNull { it.name.toLowerCase() == id.toLowerCase() }
+    }
 
+fun User.toUser() = bot.entity.User(this.idLong.toString(), this.name)
